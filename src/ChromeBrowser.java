9e13edefd676424a51c3bd72aed3d6a9600711df
Kuntal Goswami <pgroup07@gmail.com>
2023-06-20 00:07:34 +0100

    Week_11 Homework done

---
+++ src/ChromeBrowser.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowser {
+    public static void main(String[] args) throws InterruptedException {
+        //Setup Chrome browser
+
+        String baseUrl = "https://www.saucedemo.com/";
+        // Launch the Chrome Browser
+        WebDriver driver = new ChromeDriver();
+        //open The URL into the Browser
+        driver.get(baseUrl);
+        //Maximise the Browser
+        driver.manage().window().maximize();
+        // Print the title of the page
+        System.out.println("Page Title is : " + driver.getTitle());
+        //Print the current url
+        System.out.println("Current URL is : " + driver.getCurrentUrl());
+        //Print the page source
+        System.out.println("Page Source is : " + driver.getPageSource());
+        Thread.sleep(1000);
+        // Find and Enter the email to email field
+        driver.findElement(By.name("user-name")).sendKeys("Kspw11@ymail.com");
+        Thread.sleep(2000);
+
+        // find and Enter the password to password field
+        driver.findElement(By.name("password")).sendKeys("LandRover");
+        // Close the Browser
+        driver.close();
+    }
+}
